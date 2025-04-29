b73a49879cbaa44882d318cb415c4b7240287e0c
Hamad Alaslani <hamadsmma@gmail.com>
2025-04-29 16:07:22 +0300

    Finish display func to display numbers on screen

--- script.js
+++ script.js
@@ -3,10 +3,59 @@ let secondNumber;
 let operator;
 
 function display() {
+  let result = document.querySelector(".screen");
   let btns = document.querySelectorAll(".number a");
+  let number = "";
+
   btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       e.preventDefault();
+      switch (btn.textContent) {
+        case "0":
+          number += "0";
+          result.textContent = number;
+          break;
+        case ".":
+          number += ".";
+          result.textContent = number;
+        case "1":
+          number += "1";
+          result.textContent = number;
+          break;
+        case "2":
+          number += "2";
+          result.textContent = number;
+          break;
+        case "3":
+          number += "3";
+          result.textContent = number;
+          break;
+        case "4":
+          number += "4";
+          result.textContent = number;
+          break;
+        case "5":
+          number += "5";
+          result.textContent = number;
+          break;
+        case "6":
+          number += "6";
+          result.textContent = number;
+          break;
+        case "7":
+          number += "7";
+          result.textContent = number;
+          break;
+        case "8":
+          number += "8";
+          result.textContent = number;
+          break;
+        case "9":
+          number += "9";
+          result.textContent = number;
+          break;
+      }
+      console.log(number);
     });
   });
 }
